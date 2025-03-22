055c2566392f8b8735659caa8b593c15f93649f9
Marcio Flavio <oi@marcioflavio.com>
2025-03-22 19:30:37 +0100

    Add CORS policy

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('../routes');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        const allowedOrigins = (process.env.FRONTEND_URL || '*').split(',');
+        if (!origin || allowedOrigins.indexOf(origin) !== -1 || allowedOrigins.includes('*')) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 
 // Middleware to parse JSON requests
 app.use(express.json());
