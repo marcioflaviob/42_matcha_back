89201f4510a9adcbe84a964a7b097c9184b5a840
Marcio Flavio <oi@marcioflavio.com>
2025-04-13 19:57:40 +0200

    Improve UserInteractions

--- controllers/UserInteractionsController.js
+++ controllers/UserInteractionsController.js
@@ -4,7 +4,7 @@ exports.getLikeCountByUserId = async (req, res) => {
 	try {
 		const userId = req.user.id;
 		const likeCount = await UserInteractionsService.getLikeCountByUserId(userId);
-		res.send(likeCount);
+		res.status(200).send(likeCount);
 	} catch (err) {
 		res.status(404).send({ error: err.message });
 	}
@@ -15,7 +15,7 @@ exports.likeUser = async (req, res) => {
 		const userId = req.user.id;
 		const likedUserId = req.params.id;
 		const likeResult = await UserInteractionsService.likeUser(userId, likedUserId);
-		res.send(likeResult);
+		res.status(200).send(likeResult);
 	} catch (err) {
 		res.status(404).send({ error: err.message });
 	}
@@ -26,7 +26,7 @@ exports.seeProfile = async (req, res) => {
 		const userId = req.user.id;
 		const profileUserId = req.params.id;
 		const seeProfileResult = await UserInteractionsService.seeProfile(userId, profileUserId);
-		res.send(seeProfileResult);
+		res.status(200).send(seeProfileResult);
 	} catch (err) {
 		res.status(404).send({ error: err.message });
 	}
@@ -36,7 +36,7 @@ exports.getProfileViewsByUserId = async (req, res) => {
 	try {
 		const userId = req.user.id;
 		const profileViews = await UserInteractionsService.getProfileViewsByUserId(userId);
-		res.send(profileViews);
+		res.status(200).send(profileViews);
 	} catch (err) {
 		res.status(404).send({ error: err.message });
 	}
@@ -47,7 +47,7 @@ exports.matchUsers = async (req, res) => {
 		const userId = req.user.id;
 		const matchedUserId = req.params.id;
 		const matchResult = await UserInteractionsService.matchUsers(userId, matchedUserId);
-		res.send(matchResult);
+		res.status(200).send(matchResult);
 	} catch (err) {
 		res.status(404).send({ error: err.message });
 	}
@@ -63,12 +63,22 @@ exports.getMatchesByUserId = async (req, res) => {
 	}
 }
 
+exports.getPotentialMatches = async (req, res) => {
+	try {
+		const userId = req.user.id;
+		const potentialMatches = await UserInteractionsService.getPotentialMatches(userId);
+		res.status(200).send(potentialMatches);
+	} catch (err) {
+		res.status(404).send({ error: err.message });
+	}
+}
+
 exports.blockUser = async (req, res) => {
 	try {
 		const userId = req.user.id;
 		const blockedUserId = req.params.id;
 		const blockResult = await UserInteractionsService.blockUser(userId, blockedUserId);
-		res.send(blockResult);
+		res.status(200).send(blockResult);
 	} catch (error) {
 		throw new Error('Failed to block user');
 	}
@@ -78,7 +88,7 @@ exports.getBlockedUsersByUserId = async (req, res) => {
 	try {
 		const userId = req.user.id;
 		const blockedUsers = await UserInteractionsService.getBlockedUsersByUserId(userId);
-		res.send(blockedUsers);
+		res.status(200).send(blockedUsers);
 	} catch (err) {
 		res.status(404).send({ error: err.message });
 	}
